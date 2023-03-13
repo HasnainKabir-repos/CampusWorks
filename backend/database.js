143c7796a90b1086fe0ce17f96077d78af733702
Mukit Mahdin <88025578+mahdin70@users.noreply.github.com>
2023-03-13 23:59:31 +0600

    Chat Feature backend

--- backend/database.js
+++ backend/database.js
@@ -7,9 +7,9 @@ module.exports = () => {
     try{
         mongoose.connect("mongodb+srv://hasnainkabir120:nbspl2@campusworks.ommmdwm.mongodb.net/?retryWrites=true&w=majority", connectionParams);
         console.log('Connected to database')
-    }catch(error){
-        console.log(error);
-        console.log('Error connecting to database')
     }
-    
+    catch(error){
+        console.log(error);
+        console.log('Error connecting to database');
+    }   
 }
