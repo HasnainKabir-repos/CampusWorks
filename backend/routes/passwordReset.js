2528dfc6b345250a21338aa50a0fe400c3b14657
Mukit Mahdin <88025578+mahdin70@users.noreply.github.com>
2023-04-17 12:20:03 +0600

    Solved Sending Email Issue Using Ethereal

--- backend/routes/passwordReset.js
+++ backend/routes/passwordReset.js
@@ -9,90 +9,98 @@ const bcrypt = require("bcrypt");
 
 // send password link
 router.post("/", async (req, res) => {
-	try {
-		const emailSchema = Joi.object({
-			email: Joi.string().email().required().label("Email"),
-		});
-		const { error } = emailSchema.validate(req.body);
-		if (error)
-			return res.status(400).send({ message: error.details[0].message });
+  try {
+    const emailSchema = Joi.object({
+      email: Joi.string().email().required().label("Email"),
+    });
+    const { error } = emailSchema.validate(req.body);
+    if (error)
+      return res.status(400).send({ message: error.details[0].message });
 
-		let user = await User.findOne({ email: req.body.email });
-		if (!user)
-			return res
-				.status(409)
-				.send({ message: "User with given email does not exist!" });
+    let user = await User.findOne({ email: req.body.email });
+    if (!user)
+      return res
+        .status(409)
+        .send({ message: "User with given email does not exist!" });
 
-		let token = await Token.findOne({ userId: user._id });
-		if (!token) {
-			token = await new Token({
-				userId: user._id,
-				token: crypto.randomBytes(32).toString("hex"),
-			}).save();
-		}
-        
+    let token = await Token.findOne({ userId: user._id });
+    if (!token) {
+      token = await new Token({
+        userId: user._id,
+        token: crypto.randomBytes(32).toString("hex"),
+      }).save();
+    }
 
-		const url = `${process.env.BASE_URL}password-reset/${user._id}/${token.token}/`;
-		await sendEmail(user.email, "Password Reset", url);
+    const resetPasswordUrl = `${process.env.BASE_URL}password-reset/${user._id}/${token.token}/`;
 
-		res
-			.status(200)
-			.send({ message: "Password reset link sent to your email account" });
-	} catch (error) {
-		res.status(500).send({ message: "Internal Server Error" });
-	}
+    const emailBody = `<p>Hello,</p>
+    <p>We received a request to reset the password for your account.</p>
+    <p>Please click <a href="${resetPasswordUrl}">here</a> to set a new password.</p>
+    <p>If you did not request a password reset, please ignore this email.</p>
+    <p>Best,</p>
+    <p>Team CampusWorks</p>`;
+
+    await sendEmail(user.email, "Password Reset", emailBody);
+
+    res
+      .status(200)
+      .send({ message: "Password reset link sent to your email account" });
+  } catch (error) {
+    console.log(error); // Add this line to log any errors
+    res.status(500).send({ message: "Internal Server Error" });
+  }
 });
 
 // verify password reset link
 router.get("/:id/:token", async (req, res) => {
-	try {
-		const user = await User.findOne({ _id: req.params.id });
-		if (!user) return res.status(400).send({ message: "Invalid link" });
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    if (!user) return res.status(400).send({ message: "Invalid link" });
 
-		const token = await Token.findOne({
-			userId: user._id,
-			token: req.params.token,
-		});
-		if (!token) return res.status(400).send({ message: "Invalid link" });
+    const token = await Token.findOne({
+      userId: user._id,
+      token: req.params.token,
+    });
+    if (!token) return res.status(400).send({ message: "Invalid link" });
 
-		res.status(200).send("Valid Url");
-	} catch (error) {
-		res.status(500).send({ message: "Internal Server Error" });
-	}
+    res.status(200).send("Valid Url");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
 });
 
-//  set new password
 router.post("/:id/:token", async (req, res) => {
-	try {
-		const passwordSchema = Joi.object({
-			password: passwordComplexity().required().label("Password"),
-		});
-		const { error } = passwordSchema.validate(req.body);
-		if (error)
-			return res.status(400).send({ message: error.details[0].message });
+  try {
+    const passwordSchema = Joi.object({
+      password: passwordComplexity().required().label("Password"),
+    });
+    const { error } = passwordSchema.validate(req.body);
+    if (error)
+      return res.status(400).send({ message: error.details[0].message });
 
-		const user = await User.findOne({ _id: req.params.id });
-		if (!user) return res.status(400).send({ message: "Invalid link" });
+    const user = await User.findOne({ _id: req.params.id });
+    if (!user) return res.status(400).send({ message: "Invalid link" });
 
-		const token = await Token.findOne({
-			userId: user._id,
-			token: req.params.token,
-		});
-		if (!token) return res.status(400).send({ message: "Invalid link" });
+    const token = await Token.findOne({
+      userId: user._id,
+      token: req.params.token,
+    });
+    if (!token) return res.status(400).send({ message: "Invalid link" });
 
-		if (!user.verified) user.verified = true;
+    if (!user.verified) user.verified = true;
 
-		const salt = await bcrypt.genSalt(Number(process.env.SALT));
-		const hashPassword = await bcrypt.hash(req.body.password, salt);
+    const salt = await bcrypt.genSalt(Number(process.env.SALT));
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-		user.password = hashPassword;
-		await user.save();
-		await token.remove();
+    user.password = hashPassword;
+    await user.save();
+    await Token.deleteOne({ _id: token._id });
 
-		res.status(200).send({ message: "Password reset successfully" });
-	} catch (error) {
-		res.status(500).send({ message: "Internal Server Error" });
-	}
+    res.status(200).send({ message: "Password reset successfully" });
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
--- frontend/src/components/PasswordReset/index.jsx
+++ frontend/src/components/PasswordReset/index.jsx
@@ -4,71 +4,79 @@ import axios from "axios";
 import styles from "./styles.module.css";
 
 const PasswordReset = () => {
-	const [validUrl, setValidUrl] = useState(false);
-	const [password, setPassword] = useState("");
-	const [msg, setMsg] = useState("");
-	const [error, setError] = useState("");
-	const param = useParams();
-	const url = `http://localhost:8080/api/password-reset/${param.id}/${param.token}`;
+  const [validUrl, setValidUrl] = useState(false);
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+  const [error, setError] = useState("");
+  const param = useParams();
+  const url = `http://localhost:8080/api/password-reset/${param.id}/${param.token}`;
 
-	useEffect(() => {
-		const verifyUrl = async () => {
-			try {
-				await axios.get(url);
-				setValidUrl(true);
-			} catch (error) {
-				setValidUrl(false);
-			}
-		};
-		verifyUrl();
-	}, [param, url]);
+  useEffect(() => {
+    const verifyUrl = async () => {
+      try {
+        await axios.get(url);
+        setValidUrl(true);
+      } catch (error) {
+        setValidUrl(false);
+      }
+    };
+    verifyUrl();
+  }, [param, url]);
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			const { data } = await axios.post(url, { password });
-			setMsg(data.message);
-			setError("");
-			window.location = "/login";
-		} catch (error) {
-			if (
-				error.response &&
-				error.response.status >= 400 &&
-				error.response.status <= 500
-			) {
-				setError(error.response.data.message);
-				setMsg("");
-			}
-		}
-	};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(url, { password });
+      setMsg(data.message);
+      setError("");
+      window.location = "/login";
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+        setMsg("");
+      }
+    }
+  };
 
-	return (
-		<Fragment>
-			{validUrl ? (
-				<div className={styles.container}>
-					<form className={styles.form_container} onSubmit={handleSubmit}>
-						<h1>Add New Password</h1>
-						<input
-							type="password"
-							placeholder="Password"
-							name="password"
-							onChange={(e) => setPassword(e.target.value)}
-							value={password}
-							required
-							className={styles.input}
-						/>
-						{error && <div className={styles.error_msg}>{error}</div>}
-						{msg && <div className={styles.success_msg}>{msg}</div>}
-						<button type="submit" className={styles.green_btn}>
-							Submit
-						</button>
-					</form>
-				</div>
-			) : (
-				<h1>404 Not Found</h1>
-			)}
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      {validUrl ? (
+        <div className={styles.container}>
+          <form className={styles.form_container} onSubmit={handleSubmit}>
+             <h4 class="text-2xl font-bold font-poppins">Add New Password</h4>
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              required
+              className={styles.input}
+            />
+            {error && <div className={styles.error_msg}>{error}</div>}
+            {msg && <div className={styles.success_msg}>{msg}</div>}
+            <button
+              type="submit"
+              className={
+                "border-none outline-none py-3 px-4 hover:shadow-xl hover:shadow-black-100 " +
+                "text-black rounded-md w-48 font-small text-base cursor-pointer font-poppins " +
+                "shadow-md my-4 bg-teal-400"
+              }
+              style={{ backgroundColor: "#4FFFB0" }}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      ) : (
+        <h1>404 Not Found</h1>
+      )}
+    </Fragment>
+  );
 };
 
 export default PasswordReset;
